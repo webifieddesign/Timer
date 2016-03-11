b077388d27a82ba65347d501c8ea50d7d8a8a8e6
Chris Shaughnessy <chris@webifieddesign.com>
2016-03-11 17:44:30 -0500

    Initial Commit

---
+++ Timer1.2Tests/Timer1_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Timer1_2Tests.swift
+//  Timer1.2Tests
+//
+//  Created by Chris Shaughnessy on 3/11/16.
+//  Copyright © 2016 Webified Design. All rights reserved.
+//
+
+import XCTest
+@testable import Timer1_2
+
+class Timer1_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
